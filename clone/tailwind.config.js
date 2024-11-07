cf355c8813af899ca62161bcb2914f4d20965e4c
WebDevByAwaisRafiq <webdevbyawais@gmail.com>
2024-11-07 22:00:39 +0500

    Add files via upload

---
+++ clone/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        spacing:{
+        '14':'3.5rem',
+        '68':'16rem',
+        '92': '23rem'
+        
+        },
+      extend: {},
+    },
+    plugins: [],
+  }
